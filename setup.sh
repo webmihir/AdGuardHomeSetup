f12c899b79bc4e2bba90778e84a7eb210023c1b2
Mihir Gandhi <webmihir@gmail.com>
2021-08-01 18:53:49 +0000

    Re-arrange packages

--- setup.sh
+++ setup.sh
@@ -9,5 +9,20 @@ fi
 
 CUR_DIR=`dirname "$(readlink -f "$0")"`
 
-$CUR_DIR/initial_setup.sh
+echo "Downloading scripts from github into $HOME/github/ directory ..."
+if [ -d "$HOME/github/AdGuardHomeSetup" ]
+then
+	echo "  - Deleting existing $HOME/github/AdGuardHomeSetup directory."
+	rm -rf "$HOME/github/AdGuardHomeSetup" >/dev/null 2>&1
+fi
+mkdir -p $HOME/github/ >/dev/null 2>&1
+pushd $HOME/github/
+echo "DO NOT DELETE THIS DIRECTORY AND ITS CONTENTS. THEY ARE USED IN CRON JOBS TO ENSURE YOUR INSTALLATION STAYS UP TO DATE." > "DO_NOT_DELETE"
+git clone https://github.com/webmihir/AdGuardHomeSetup.git >/dev/null 2>&1
+
+echo "Running Initial Setup to install required packages"
+$HOME/github/AdGuardHomeSetup/packages/install.sh
+
+
+popd
 
