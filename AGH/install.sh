ae46fe6979ed9004a12b0e94a8b5d54577d15a06
Mihir Gandhi <webmihir@gmail.com>
2021-08-02 14:56:33 +0000

    Remove hardcoded yaml file for adguardhome setup

--- AGH/install.sh
+++ AGH/install.sh
@@ -6,14 +6,12 @@ echo "Installing AdGuardHome Beta ...."
 AGH_HOME=/opt/AdGuardHome
 if [ -f "$AGH_HOME/AdGuardHome" ]
 then
+	echo "Uninstalling current AdGuardHome installation first..."
 	$AGH_HOME/AdGuardHome -s stop
 	curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -u
 fi
 
-AGH_PASS=`htpasswd -nbB admin '$AGH_PASSWORD' | tr -d 'admin:'`
 curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -c beta
-$AGH_HOME/AdGuardHome -s stop >/dev/null 2>&1
-
-cat $SCRIPT_DIR/AGH/AdGuardHome.yaml | tr -s 'ENTERPASSWORDHERE' "$AGH_PASSWORD"> $AGH_HOME/AdGuardHome.yaml
+curl 'http://localhost:3000/control/install/configure' -H 'Content-Type: application/json' --data-raw '{"web": {"ip":"0.0.0.0", "port":8001, "status": "", "can_autofix":false}, "dns": {"ip":"0.0.0.0", "port": 53, "status":"", "can_autofix":false}, "username":"admin", "password":"'$AGH_PASSWORD'"}'
 
 $AGH_HOME/AdGuardHome -s start
