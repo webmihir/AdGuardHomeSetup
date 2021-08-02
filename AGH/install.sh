031a5a166eee0fee0781a6f3e9c165a4a5ee0355
Mihir Gandhi <webmihir@gmail.com>
2021-08-02 16:33:22 +0000

    Update scripts

--- AGH/install.sh
+++ AGH/install.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
-source $SCRIPT_DIR/common/lib.sh
+source "$SCRIPT_DIR/common/lib.sh"
 
-AGH_USERNAME="$2"
-AGH_PASSWORD="$4"
+while getopts u:p: flag
+do
+	case $flag in
+		u) AGH_USERNAME="$OPTARG";;
+		p) AGH_PASSWORD="$OPTARG";;
+	esac
+done
 
-echo "Installing AdGuardHome Beta ...."
+if [[ -z "$AGH_USERNAME" || -z "$AGH_PASSWORD" ]]
+then
+	echo "Username and Password must not be empty."
+	exit 1
+fi
+
 AGH_HOME=/opt/AdGuardHome
 if [ -f "$AGH_HOME/AdGuardHome" ]
 then
 	echo "Uninstalling current AdGuardHome installation first..."
-	$AGH_HOME/AdGuardHome -s stop
-	curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -u
+	$AGH_HOME/AdGuardHome -s stop >/dev/null 2>&1
+	curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -u >/dev/null 2>&1
 fi
 
-curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -c beta
+echo "Installing AdGuardHome Beta ..."
+curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -c beta >/dev/null 2>&1
+
+echo "Waiting for AdGuardHome Service to start ..."
+# TODO: Change this to poll port 3000 until 2xx response code is received
 sleep 10
+
+echo "Creating initial configuration for AdGuardHome Service ..."
 curl 'http://localhost:3000/control/install/configure' -H 'Content-Type: application/json' --data-raw '{"web": {"ip":"0.0.0.0", "port":8001, "status": "", "can_autofix":false}, "dns": {"ip":"0.0.0.0", "port": 53, "status":"", "can_autofix":false}, "username":"'$AGH_USERNAME'", "password":"'$AGH_PASSWORD'"}'
 
--- setup.sh
+++ setup.sh
@@ -7,16 +7,46 @@ then
         exit 1
 fi
 
+show_help() {
+	echo "Usage:"
+	echo "$0 -u \"<AdGuardHome Username>\" -p \"<AdGuardHome Password>\" -a 1 -w 1"
+	echo "   -u: AdGuardHome Username. Default: \"admin\""
+	echo "   -p: AdGuardHome Password. Mandatory Parameter must be supplied if \"-a 1\" flag is used."
+	echo "   -a: 0 (do not install AdGuardHome) or 1 (Install AdGuardHome). Default=1."
+	echo "   -w: 0 (do not install WireGuard) or 1 (Install WireGuard). Default=1."
+	echo ""
+	echo "Example:"
+	echo "  $0 -u \"admin\" -p \"P@ssw0rd\""
+}
+
 export CUR_DIR=`dirname "$(readlink -f "$0")"`
 export INSTALL_DIR=$HOME/github
 export SCRIPT_DIR=$INSTALL_DIR/AdGuardHomeSetup
 
-# TODO: Change these to query parameters
 AGH_USERNAME="admin"
-AGH_PASSWORD="1234"
 INSTALL_AGH=1
 INSTALL_SSL=1
 INSTALL_WIREGUARD=1
+
+while getopts u:p:a:w:h flag
+do
+	case "${flag}" in
+		u) AGH_USERNAME="${OPTARG}";;
+		p) AGH_PASSWORD="${OPTARG}";;
+		a) INSTALL_AGH=$OPTARG;;
+		w) INSTALL_WIREGUARD=$OPTARG;;
+		h) show_help; exit 0;;
+	esac
+done
+if [ $INSTALL_AGH -eq 1 ]
+then
+	if [ -z "$AGH_PASSWORD" ]
+	then
+		echo "No Password set for AdGuardHome using -p flag."
+		show_help
+		exit 1
+	fi
+fi
 
 echo "Downloading scripts from github into $HOME/github/ directory ..."
 if [ -d "$SCRIPT_DIR" ]
